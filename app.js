474f648b622913c04fd8e05859edb22b8bd34ec3
Ronnel Chavez Divinagracia <102518104+Byenuhree@users.noreply.github.com>
2023-11-04 15:04:45 +0800

    js is working

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let color = "black";
 let click = true;
+let color = "black";
+const colorPicker = document.getElementById("colorPicker");
+const rangeInput = document.getElementById("rangeInput");
+const outputElement = document.getElementById("output");
 
 function populateBoard(size) {
   let board = document.querySelector(".sketch-board");
-  let squares = document.querySelectorAll("div");
+  let squares = board.querySelectorAll("div");
   squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
@@ -19,19 +22,28 @@ function populateBoard(size) {
 
 populateBoard(16);
 
+// Add an event listener to the range input
+rangeInput.addEventListener("input", function () {
+  const value = rangeInput.value;
+  outputElement.textContent = value;
+});
+
 function changeSize(input) {
-  if (input >= 2 && input <= 100) {
-    document.querySelector(".error").style.display = "none";
-    populateBoard(input);
-  } else {
-    document.querySelector(".error").style.display = "flex";
-  }
+  // if (input >= 2 && input <= 100) {
+  //   document.querySelector(".error").style.display = "none";
+  populateBoard(input);
+  // } else {
+  //   document.querySelector(".error").style.display = "flex";
+  // }
 }
 
 function colorSquare() {
   if (click) {
     if (color === "random") {
       this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else if (color === "selected") {
+      const selectedColor = colorPicker.value;
+      this.style.backgroundColor = selectedColor;
     } else {
       this.style.backgroundColor = color;
     }
